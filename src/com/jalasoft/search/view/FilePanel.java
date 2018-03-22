c654fad175c3753f415f84a76b6fec63ac34ecba
monica pardo <mpardo@pros.com>
2018-03-21 22:57:36 -0300

    -Add the Main class -Update the classes for UI based in the code conventions -Add the UI components for the Search Options section

--- src/com/jalasoft/search/view/FilePanel.java
+++ src/com/jalasoft/search/view/FilePanel.java
@@ -1,8 +1,30 @@
+/*
+ *     1 21/03/18
+ *
+ * Copyright (c) 2018 Sun Microsystems, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Sun
+ * Microsystems, Inc. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ *  with Sun.
+ *
+ */
+
 package com.jalasoft.search.view;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.GridLayout;
 
+/**
+ *
+ This class represent the panel that will contains the results and search criteria options for the File
+ *
+ * @version
+1.0 21 Mar 2018  * @author
+Monica Pardo */
 
 public class FilePanel extends JPanel{
     private JPanel searchPanelCriteria;
@@ -10,15 +32,18 @@ public class FilePanel extends JPanel{
 
     public FilePanel(){
         super();
+        initializeDialog();
+
+    }
+    //This method allows to initialize the Panel
+    public void initializeDialog(){
         setBackground(Color.white);
-        this.setLayout(new GridLayout(1,2));
+        this.setLayout(new GridLayout(1, 2));
         searchPanelCriteria=new SearchPanelCriteria();
         searchPanelResults=new SearchPanelResults();
         this.add(searchPanelCriteria);
         this.add(searchPanelResults);
-
     }
 
-
-
 }
+
