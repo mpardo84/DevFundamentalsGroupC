ca624c11b7ebe696ab482e0f4a6ed4cafcd378ad
Gretta Rocha Tapia <Gretta Rocha@jalasoft.local>
2018-03-27 08:49:04 -0400

    Complete copyright seccion in all files

--- src/com/jalasoft/search/controller/SearchController.java
+++ src/com/jalasoft/search/controller/SearchController.java
@@ -1,3 +1,16 @@
+/*
+ *      26/03/18
+ *
+ * Copyright (c) 2018 Sun Microsystems, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Sun
+ * Microsystems, Inc. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ *  with Sun.
+ *
+ */
 package com.jalasoft.search.controller;
 
 import com.jalasoft.search.model.Search;
